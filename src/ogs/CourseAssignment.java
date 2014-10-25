1ee4c898a80f66b6d63f76dc34c76bafd7b4ccee
zainul101 <zainul101@10.1.1.12>
2014-10-25 13:57:44 -0400

    Using GUI SnapShot to locate source code.

---
+++ src/ogs/CourseAssignment.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ogs;
+
+/**
+ *
+ * @author zainul101
+ */
+public class CourseAssignment 
+{
+    private int numberOfAssignment;
+    
+    /**
+     * Get Points Possible Method
+     */
+    public void getPointsPossible()
+    {
+        
+    }
+    
+    /**
+     * Get Points Grades method
+     */
+    public void getPointsGrades()
+    {
+        
+    }
+}
---
+++ src/ogs/Professor.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ogs;
+
+/**
+ *
+ * @author zainul101
+ */
+public class Professor extends Person 
+{
+    private String officeHours;
+    
+    
+    /**
+     * Appoint TA method
+     */
+    public void appointTA()
+    {
+        
+    }
+    
+    /**
+     * Create Assignment method
+     */
+    
+    /**
+     * View Grade Statistic method
+     */
+    public void viewGradeStatistic()
+    {
+        
+    }
+    
+    /**
+     * View Report method
+     */
+    public void viewReport()
+    {
+        
+    }
+    
+}
